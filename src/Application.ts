0fb0f09b28a92a5f447511a26d2c45e86d3faae9
moonstar-x <christian.lopez99@outlook.com>
2023-01-29 20:54:37 -0500

    Created Application class.

--- src/Application.ts
+++ src/Application.ts
@@ -1,12 +1,35 @@
 import { EventEmitter } from 'events';
 import type { Workflow } from './workflow/Workflow';
+import { WebhookManager, WebhookManagerOptions } from './workflow/triggers/webhook/WebhookManager';
+
+export interface ApplicationOptions {
+  webhookManager: WebhookManagerOptions
+}
 
 export class Application extends EventEmitter {
-  public async registerWorkflow(workflow: Workflow): Promise<void> {
-    await workflow.setup();
+  private _webhookManager: WebhookManager;
+
+  constructor(options: ApplicationOptions) {
+    super();
+    this._webhookManager = new WebhookManager(options.webhookManager);
+    this.registerEvents();
   }
 
-  public registerWorkflows(workflows: Workflow[]): Promise<void[]> {
-    return Promise.all(workflows.map((workflow) => this.registerWorkflow(workflow)));
+  private registerEvents() {
+    this.on('workflowStart', () => console.log('started'));
+    this.on('workflowFinish', () => console.log('ended'));
+    this.on('workflowError', () => console.log('error'));
+  }
+
+  public registerWorkflow(workflow: Workflow): void {
+    workflow.setup();
+  }
+
+  public registerWorkflows(workflows: Workflow[]): void {
+    workflows.map((workflow) => this.registerWorkflow(workflow));
+  }
+
+  get webhookManager() {
+    return this._webhookManager;
   }
 }
