de755323a836184b5f83de4ce48fa06c99d23449
moonstar-x <christian.lopez99@outlook.com>
2023-01-29 22:29:35 -0500

    Added metadata for test workflows.

--- src/workflow/impl/ErrorWorkflow.ts
+++ src/workflow/impl/ErrorWorkflow.ts
@@ -1,10 +1,19 @@
 import { Workflow } from '../Workflow';
+import { Application } from './../../Application';
+import { WebhookTrigger } from './../triggers/webhook/WebhookTrigger';
 
 interface TriggerPayload {
   error: string
 }
 
 export class ErrorWorkflow extends Workflow<TriggerPayload> {
+  constructor(application: Application, trigger: WebhookTrigger<TriggerPayload>) {
+    super(application, trigger, {
+      name: 'ErrorWorkflow',
+      description: 'A workflow that throws an error.'
+    });
+  }
+
   public async run(payload?: TriggerPayload): Promise<void> {
     console.log(payload);
     throw new Error('Oops...');
--- src/workflow/impl/TestWorkflow.ts
+++ src/workflow/impl/TestWorkflow.ts
@@ -1,10 +1,18 @@
 import { Workflow } from '../Workflow';
-
+import { Application } from './../../Application';
+import { WebhookTrigger } from './../triggers/webhook/WebhookTrigger';
 interface TriggerPayload {
   name: string
 }
 
 export class TestWorkflow extends Workflow<TriggerPayload> {
+  constructor(application: Application, trigger: WebhookTrigger<TriggerPayload>) {
+    super(application, trigger, {
+      name: 'TestWorkflow',
+      description: 'A testing workflow.'
+    });
+  }
+
   public async run(payload?: TriggerPayload): Promise<void> {
     console.log(payload);
     console.log('Testing workflow...');
