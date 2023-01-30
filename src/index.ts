3a74fec00472bf6be58afc6690bde7d2cc9d6c10
moonstar-x <christian.lopez99@outlook.com>
2023-01-29 20:54:54 -0500

    Created app entrypoint.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-// import { Application } from './Application';
+import { Application } from './Application';
+import { TestWorkflow } from './workflow/impl/TestWorkflow';
+import { ErrorWorkflow } from './workflow/impl/ErrorWorkflow';
 
-// const app = new Application();
+const main = async () => {
+  const app = new Application({
+    webhookManager: {
+      port: 4000
+    }
+  });
+
+  app.registerWorkflows([
+    new TestWorkflow(app, app.webhookManager.createTrigger('test')),
+    new ErrorWorkflow(app, app.webhookManager.createTrigger('error'))
+  ]);
+
+  app.webhookManager.start();
+};
+
+main();
