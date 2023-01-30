4dad07742ed9cac4c74faad914b88e4e449ae0c2
moonstar-x <christian.lopez99@outlook.com>
2023-01-29 21:26:27 -0500

    Created route middleware.

---
+++ src/express/middleware/route.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { MethodNotAllowedError, ResourceNotFoundError, BadRequestError } from '../errors';
+
+type HTTP_METHODS = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD';
+
+export const onlySupportedMethods = (...methods: HTTP_METHODS[]) => {
+  return (_: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Methods', `${methods.join(' ')} OPTIONS`);
+    next(new MethodNotAllowedError());
+  };
+};
+
+export const routeNotFound = (_req: Request, _res: Response, next: NextFunction) => {
+  next(new ResourceNotFoundError('This route is not handled by the server.'));
+};
+
+export const jsonBodyRequired = (required: boolean) => (req: Request, _res: Response, next: NextFunction) => {
+  if (!required) {
+    next();
+    return;
+  }
+
+  if (!req.body || Object.keys(req.body).length < 1) {
+    next(new BadRequestError('A JSON body is required.'));
+    return;
+  }
+
+  next();
+};
