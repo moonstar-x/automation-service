6168bf4fec130315ee52541e8daddbe46798fd2f
moonstar-x <christian.lopez99@outlook.com>
2023-02-02 22:02:28 -0500

    Implemented TwitterTrigger.

--- src/workflow/impl/TestWorkflow.ts
+++ src/workflow/impl/TestWorkflow.ts
@@ -1,44 +1,18 @@
+import { TweetV2SingleStreamResult } from 'twitter-api-v2';
 import { Workflow } from '../Workflow';
 import { Application } from './../../Application';
-import { TimeoutTrigger } from './../triggers/TimeoutTrigger';
-import { levelDatabaseService } from '../../services/LevelDatabaseService';
-import * as Twitter from '../../clients/twitter';
+import { TwitterTrigger } from './../triggers/TwitterTrigger';
 import { twitter } from '../../../config/config.json';
 
-export class TestWorkflow extends Workflow<void> {
-  private twitterClient: Twitter.ClientV1;
-
+export class TestWorkflow extends Workflow<TweetV2SingleStreamResult> {
   constructor(application: Application) {
-    super(application, new TimeoutTrigger(5), {
+    super(application, new TwitterTrigger(twitter.bearer_token, ['moonstar_x99']), {
       name: 'TestWorkflow',
       description: 'Testing stuff'
     });
-
-    this.twitterClient = new Twitter.ClientV1({
-      appKey: twitter.api_key,
-      appSecret: twitter.api_key_secret
-    });
   }
 
-  public override async setup(): Promise<void> {
-    await super.setup();
-
-    const username = 'moonstar_x99';
-    const credentials = await levelDatabaseService.get<Twitter.Types.OAuthV1Tokens>(`twitter:creds:v1:${username}`);
-
-    if (!credentials) {
-      throw new Error(`No credentials found for Twitter account ${username}`);
-    }
-
-    this.twitterClient.login(credentials);
-  }
-
-  public async run(): Promise<void> {
-    await this.twitterClient.tweet('Testing stuff', {
-      place: {
-        lat: -82.8628,
-        long: 135.0000
-      }
-    });
+  public async run(payload: TweetV2SingleStreamResult): Promise<void> {
+    console.log(payload);
   }
 }
---
+++ src/workflow/triggers/TwitterTrigger.ts
@@ -0,0 +1,93 @@
+import { TwitterApi, TweetStream, TweetV2SingleStreamResult, ETwitterStreamEvent } from 'twitter-api-v2';
+import { Logger } from '../../utils/logging';
+import { Trigger, Clearable } from '../Trigger';
+
+export class TwitterTrigger extends Trigger<TweetV2SingleStreamResult> implements Clearable {
+  private client: TwitterApi;
+  private stream: TweetStream<TweetV2SingleStreamResult> | null;
+  private usernamesToFollow: string[];
+  private logger: Logger;
+  private streamValue: string;
+
+  constructor(bearerToken: string, usernamesToFollow: string[]) {
+    super();
+    
+    if (!usernamesToFollow.length) {
+      throw new Error('At least one user needs to be followed.');
+    }
+
+    this.client = new TwitterApi(bearerToken);
+    this.stream = null;
+    this.usernamesToFollow = usernamesToFollow;
+    this.logger = new Logger('TwitterTrigger');
+
+    this.streamValue = this.usernamesToFollow.map((username) => `from:${username}`).join(' ');
+  }
+
+  public async init(): Promise<void> {
+    const currentRules = (await this.client.v2.streamRules()).data;
+    const ruleForThisTrigger = currentRules.find((rule) => rule.tag === this.streamValue);
+
+    if (!ruleForThisTrigger) {
+      this.logger.debug('No rules found applicable for this trigger.');
+
+      const setRules = await this.client.v2.updateStreamRules({
+        add: [
+          { value: this.streamValue, tag: this.streamValue }
+        ]
+      });
+  
+      this.logger.debug('Added the following rules:', setRules);
+    }
+
+    this.stream = await this.client.v2.searchStream({
+      'user.fields': ['id', 'username'],
+      'tweet.fields': ['id', 'text', 'author_id'],
+      autoConnect: true
+    });
+    this.registerStreamEvents();
+  }
+
+  private registerStreamEvents() {
+    if (!this.stream) {
+      return;
+    }
+
+    this.stream.on(ETwitterStreamEvent.Data, (data) => {
+      if (data.matching_rules.some((rule) => rule.tag === this.streamValue)) {
+        this.emit('trigger', data);
+      }
+    });
+
+    this.stream.on(ETwitterStreamEvent.DataError, (error) => {
+      this.logger.error(error);
+    });
+
+    this.stream.on(ETwitterStreamEvent.TweetParseError, (error) => {
+      this.logger.error(error);
+    });
+
+    this.stream.on(ETwitterStreamEvent.ConnectionError, (error) => {
+      this.logger.error(error);
+      this.clear();
+    });
+
+    this.stream.on(ETwitterStreamEvent.ConnectError, (error) => {
+      this.logger.error(error);
+      this.clear();
+    });
+
+    this.stream.on(ETwitterStreamEvent.ConnectionClosed, () => {
+      this.logger.warn('Stream connection has closed.');
+    });
+
+    this.stream.on(ETwitterStreamEvent.ConnectionLost, () => {
+      this.logger.warn('Stream connection has been lost.');
+    });
+  }
+
+  public clear(): void {
+    this.stream?.close();
+    this.stream = null;
+  }
+}
