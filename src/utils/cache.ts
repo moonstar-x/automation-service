72467bb1d1a06643d17d5db22e99a7a75ed5442e
moonstar-x <christian.lopez99@outlook.com>
2023-06-23 00:21:44 -0500

    Created a ExpiringCache util.

---
+++ src/utils/cache.ts
@@ -0,0 +1,38 @@
+export type ExpiringCacheValue<T> = {
+  data: T,
+  expires_in: number
+};
+
+export class ExpiringCache<K, V> {
+  private map: Map<K, ExpiringCacheValue<V>>;
+  private ttl: number;
+
+  constructor(ttl: number) {
+    this.map = new Map();
+    this.ttl = ttl;
+  }
+
+  public set(key: K, value: V): void {
+    this.map.set(key, {
+      data: value,
+      expires_in: Date.now() + this.ttl
+    });
+  }
+
+  public has(key: K): boolean {
+    this.ensureTtl(key);
+    return this.map.has(key);
+  }
+
+  public get(key: K): V | undefined {
+    this.ensureTtl(key);
+    return this.map.get(key)?.data;
+  }
+
+  private ensureTtl(key: K): void {
+    const stored = this.map.get(key);
+    if (stored && stored.expires_in < Date.now()) {
+      this.map.delete(key);
+    }
+  }
+}
